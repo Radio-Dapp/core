7a55afb117566029e1d923bbcfb03378d7b68c4c
Styles <kartik100100@gmail.com>
2025-03-04 12:04:37 +0530

    config vercel

--- client/src/pages/dashboard/account/Account.tsx
+++ client/src/pages/dashboard/account/Account.tsx
@@ -1,9 +1,5 @@
-import { Card, CardHeader, Chip } from "@heroui/react";
 import Sidebar from "../Sidebar"
 import TopNav from "../TopNav";
-import Link from "../../../shared/components/Link";
-import { truncateString } from "../../../shared/lib/utils";
-
 
 export default function Account() {
     return (
@@ -15,56 +11,6 @@ export default function Account() {
 
                 {/* // Main content */}
                 <div className="relative sm:ml-[6.5rem] flex p-8 flex-col">
-                    <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-                        <Link className="w-full" to={`/dashboard/explore/1`} >
-                            <Card className="transition-all border cursor-pointer bg-grey-100 border-border/60 hover:border-border ">
-                                <CardHeader>
-                                    <div className="flex items-center justify-between w-full px-2 py-2">
-                                        <Chip color="warning" className="px-2 text-tiny" variant="flat">Fund Type</Chip>
-
-                                        <p className="text-grey-600">$15000</p>
-                                    </div>
-                                </CardHeader>
-
-                                <div className="flex flex-col justify-between px-6 py-5 mx-1 mb-1 transition-all bg-grey-200 rounded-xl hover:bg-grey-250">
-                                    <div>
-                                        <p className="text-grey-600">Name</p>
-                                        <small className="text-grey-800">{truncateString("Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quasi assumenda sequi tenetur provident veritati ansjdsdewj effadfdfef...", 100)}</small>
-                                    </div>
-
-                                    <div className="flex justify-between w-full mt-6 text-grey-600">
-                                        <small>Current holder: 200</small>
-                                        <small>Created At: 12/1/2025</small>
-                                    </div>
-                                </div>
-                            </Card>
-                        </Link>
-
-                        <Link className="w-full" to={`/dashboard/explore/1`} >
-                            <Card className="transition-all border cursor-pointer bg-grey-100 border-border/60 hover:border-border ">
-                                <CardHeader>
-                                    <div className="flex items-center justify-between w-full px-2 py-2">
-                                        <Chip color="warning" className="px-2 text-tiny" variant="flat">Fund Type</Chip>
-
-                                        <p className="text-grey-600">$15000</p>
-                                    </div>
-                                </CardHeader>
-
-                                <div className="flex flex-col justify-between px-6 py-5 mx-1 mb-1 transition-all bg-grey-200 rounded-xl hover:bg-grey-250">
-                                    <div>
-                                        <p className="text-grey-600">Name</p>
-                                        <small className="text-grey-800">{truncateString("Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quasi assumenda sequi tenetur provident veritati ansjdsdewj effadfdfef...", 100)}</small>
-                                    </div>
-
-                                    <div className="flex justify-between w-full mt-6 text-grey-600">
-                                        <small>Current holder: 200</small>
-                                        <small>Created At: 12/1/2025</small>
-                                    </div>
-                                </div>
-                            </Card>
-                        </Link>
-                    </div>
-
                 </div>
             </div>
         </div>
--- client/src/pages/dashboard/settings/Settings.tsx
+++ client/src/pages/dashboard/settings/Settings.tsx
@@ -9,14 +9,23 @@ export default function Settings({ }: Props) {
         <div className="relative min-h-screen w-full flex bg-grey-50">
             <Sidebar />
 
-            <div className="w-full">
+            <div className="w-full flex flex-col">
                 <TopNav />
 
                 {/* // Main content */}
-                <div className="relative sm:ml-[6.5rem] flex flex-col gap-2 p-8">
-                    <h1>Become a Manager</h1>
-                    <p className="bg-grey-200 text-sm text-grey-650 p-3 rounded-md">Become a fund manager for just <span className="text-primary">$10</span> and start creating investment funds that others can invest in! Just like traditional stock fund managers, you'll earn management fees and performance-based rewards when people buy into your fund. This is your chance to build your own investment strategy, attract investors, and generate income as your fund grows. Take control, manage your own portfolio, and start earning like a professional fund manager today!</p>
-                    <Button radius="full" variant="faded" className="w-max self-end mt-2 bg-primary text-grey-0 border-none" >Register</Button>
+                <div className="relative md:w-full max-w-2xl self-center text-grey-800 sm:ml-[6.5rem] flex flex-col gap-2 p-8">
+                    <h2 className="text-3xl font-semibold">Settings</h2>
+                    <p className="text-grey-500">Set up your application below..</p>
+
+                    <div className="border p-6 mt-4 rounded-md flex flex-col gap-2">
+                        <h2 className="text-2xl font-semibold">Become Manager</h2>
+                        <p className="text-grey-500">Become a fund manager and start creating investment funds that others can invest in! Just like traditional stock fund managers, you'll earn management fees and performance-based rewards when people buy into your fund. This is your chance to build your own investment strategy, attract investors, and generate income as your fund grows. Take control, manage your own portfolio, and start earning like a professional fund manager today!</p>
+
+                        {/* Become Manager */}
+                        <div className="flex flex-col gap-2 max-w-2xl">
+                            <Button radius="sm" variant="bordered" className="w-full bg-grey-100 self-end flex mt-2 border-border border-1" >Register</Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
